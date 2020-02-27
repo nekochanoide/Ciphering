af34522d78d96f66a48db6996e8bab49bf15e55d
nekochanoide <36743508+nekochanoide@users.noreply.github.com>
2020-02-27 21:26:20 +0500

    pepeganism

--- Module.Gamma/Model.cs
+++ Module.Gamma/Model.cs
@@ -17,6 +17,17 @@ namespace Module.Gamma
         private byte[] firstLineBytes;
         private byte[] secondLineBytes;
         private byte[] resultLineBytes;
+        private string debugLine;
+        private string groups;
+
+        public Model()
+        {
+            Encoding = Encoding.ASCII;
+            //Encoding = Encoding.Unicode;
+            //Encoding = Encoding.UTF8;
+            Random = new Random();
+            IsKeyGenerating = true;
+        }
 
         public string FirstLine
         {
@@ -30,24 +41,27 @@ namespace Module.Gamma
 
         private void KeepKey()
         {
-            while (true)
+            //while (true)
+            //{
+            var fBytes = Encoding.GetBytes(FirstLine);
+            var sBytes = Encoding.GetBytes(SecondLine ?? "");
+            var diff = fBytes.Length - sBytes.Length;
+            if (fBytes.Length > sBytes.Length)
             {
-                var fBytes = Encoding.GetBytes(FirstLine);
-                var sBytes = Encoding.GetBytes(SecondLine);
-                var diff = fBytes.Length - sBytes.Length;
-                if (fBytes.Length > sBytes.Length)
-                {
-                    var newBytes = new byte[diff];
-                    Random.NextBytes(newBytes);
-                    SecondLine += Encoding.GetString(newBytes);
-                }
-                else
-                {
-                    sBytes = sBytes.Take(sBytes.Length + diff).ToArray();
-                    SecondLine = Encoding.GetString(sBytes);
-                    break;
-                }
+                var newBytes = new byte[diff];
+                Random.NextBytes(newBytes);
+                if (Encoding.EncodingName == "US-ASCII")
+                    for (int i = 0; i < newBytes.Length; i++)
+                        newBytes[i] >>= 1;
+                SecondLine += Encoding.GetString(newBytes);
             }
+            else
+            {
+                sBytes = sBytes.Take(sBytes.Length + diff).ToArray();
+                SecondLine = Encoding.GetString(sBytes);
+                //break;
+            }
+            //}
         }
 
         public string SecondLine { get => secondLine; set => SetLine(ref secondLine, x => x.SecondLineBytes, value); }
@@ -55,9 +69,14 @@ namespace Module.Gamma
         public byte[] FirstLineBytes { get => firstLineBytes; set => SetTopBytes(ref firstLineBytes, value); }
         public byte[] SecondLineBytes { get => secondLineBytes; set => SetTopBytes(ref secondLineBytes, value); }
         public byte[] ResultLineBytes { get => resultLineBytes; set => SetResultBytes(ref resultLineBytes, value); }
+        public int? SizeOfFirst { get => FirstLineBytes?.Length; }
+        public int? SizeOfSecond { get => SecondLineBytes?.Length; }
+        public int? SizeOfResult { get => ResultLineBytes?.Length; }
         public bool IsKeyGenerating { get; set; }
-        public Encoding Encoding { get; set; } = Encoding.Unicode;
-        public Random Random { get; set; } = new Random();
+        public Encoding Encoding { get; set; }
+        public Random Random { get; set; }
+        public string DebugLine { get => debugLine; set => SetField(ref debugLine, value); }
+        public string Groups { get => groups; set => SetField(ref groups, value); }
 
         bool SetTopBytes(ref byte[] field, byte[] value, [CallerMemberName] string propertyName = null)
         {
@@ -67,13 +86,15 @@ namespace Module.Gamma
 
             if (propertyName == nameof(FirstLineBytes))
             {
-                if (value.Length == SecondLineBytes.Length)
+                if (value.Length == SecondLineBytes?.Length)
                     ResultLineBytes = DoXor(value, SecondLineBytes).ToArray();
             }
-            else if (value.Length == FirstLineBytes.Length)
+            else if (value.Length == FirstLineBytes?.Length)
                 ResultLineBytes = DoXor(value, FirstLineBytes).ToArray();
 
             NotifyPropertyChanged(propertyName);
+            NotifyPropertyChanged(nameof(SizeOfFirst));
+            NotifyPropertyChanged(nameof(SizeOfSecond));
             return true;
         }
 
@@ -96,10 +117,14 @@ namespace Module.Gamma
                 return false;
             field = value;
 
-            var expr = (MemberExpression)expression.Body;
-            var prop = (PropertyInfo)expr.Member;
+            //var expr = (MemberExpression)expression.Body;
+            //var prop = (PropertyInfo)expr.Member;
             var input = Encoding.GetBytes(value.ToString());
-            prop.SetValue(this, input, null);
+            //prop.SetValue(this, input, null);
+            if (propertyName == nameof(FirstLine))
+                FirstLineBytes = input;
+            else
+                SecondLineBytes = input;
 
             NotifyPropertyChanged(propertyName);
             return true;
--- Module.Gamma/ViewModel.cs
+++ Module.Gamma/ViewModel.cs
@@ -21,11 +21,59 @@ namespace Module.Gamma
 
         void GenerateKey()
         {
-            var bytes = Encoding.Unicode.GetBytes(Model.FirstLine);
+            var bytes = Model.Encoding.GetBytes(Model.FirstLine);
             Model.Random.NextBytes(bytes);
-            //for (int i = 0; i < bytes.Length; i++)
-            //    bytes[i] >>= 1;
+            if (Model.Encoding.EncodingName == "US-ASCII")
+                for (int i = 0; i < bytes.Length; i++)
+                    bytes[i] >>= 1;
             Model.SecondLine = Model.Encoding.GetString(bytes);
+        }
+
+        public ICommand _packToGroupCommand;
+        public ICommand PackToGroupCommand
+            => _packToGroupCommand ?? (_packToGroupCommand = new Command(GenerateGroup, 
+                () => !string.IsNullOrWhiteSpace(Model.SecondLine)));
+
+        public ICommand _unpackFromGroupCommand;
+        public ICommand UnpackFromGroupCommand
+            => _unpackFromGroupCommand ?? (_unpackFromGroupCommand = new Command(RegenerateKey, 
+                () => !string.IsNullOrWhiteSpace(Model.Groups)));
+
+        private void GenerateGroup()
+        {
+            var bytes = Model.Encoding.GetBytes(Model.SecondLine);
+            bytes = PackToGroup(bytes);
+            Model.Groups = Model.Encoding.GetString(bytes);
+        }
+
+        private void RegenerateKey()
+        {
+            var bytes = Model.Encoding.GetBytes(Model.Groups);
+            bytes = UnpackFromGroup(bytes);
+            Model.SecondLine = Model.Encoding.GetString(bytes);
+        }
+
+        static byte[] PackToGroup(byte[] bytes)
+        {
+            var r = (byte)new Random().Next(2, 9);
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] += r;
+                bytes[i] ^= r;
+            }
+            return bytes.Append(r).ToArray();
+        }
+
+        static byte[] UnpackFromGroup(byte[] bytes)
+        {
+            var r = bytes[bytes.Length - 1];
+            bytes = bytes.Take(bytes.Length - 1).ToArray();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] ^= r;
+                bytes[i] -= r;
+            }
+            return bytes;
         }
 
         private Random SetRandom(int seed) => Model.Random = new Random(seed);
